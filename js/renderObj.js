78e58d754f3cd10e16e2054052e102406059d557
TheFox <krakaka088@gmail.com>
2019-02-26 16:05:54 +0300

    compact object renders

--- js/renderObj.js
+++ js/renderObj.js
@@ -1,15 +1,32 @@
-module.exports = {
-    objSeal: function(obj, select) {
-        let object = new PIXI.Sprite(PIXI.loader.resources["assets/obj/seal.png"].texture);
-        object.x = obj.x;
-        object.y = obj.y;
-        
-        object.width *= 2
-        object.height *= 2
+/* 
+important sprites (bard, hero, npcs, etc) are ripped from the game and are hq so we use generic1x() for them
+less important sprites are scaled down 2x for compression purposes, so we scale them back up in runtime with generic2x()
+*/
 
-        return object
-    },
-    objBard: function(obj, select) {
+const generic1x = (obj, select) => { 
+    let object = new PIXI.Sprite(PIXI.loader.resources["assets/obj/"+obj.name.replace("obj","").toLowerCase()+".png"].texture)
+
+    object.x = obj.x;
+    object.y = obj.y-object.height;
+
+    return object
+}
+
+const generic2x = (obj, select) => { 
+    let object = new PIXI.Sprite(PIXI.loader.resources["assets/obj/"+obj.name.replace("obj","").toLowerCase()+".png"].texture)
+    
+    object.width *= 2
+    object.height *= 2
+
+    object.x = obj.x;
+    object.y = obj.y-object.height;
+
+    return object
+}
+
+module.exports = {
+    //special render functions are needed for non-sprite renders and renders with weird x/y/width/height properties
+    objBard: function(obj, select) { //bard has a weird y value so we add the y manually
         let object = new PIXI.Sprite(PIXI.loader.resources["assets/obj/bard.png"].texture);
         object.x = obj.x;
         object.y = obj.y-object.height/3*2;
@@ -19,33 +36,11 @@ module.exports = {
 
         return object
     },
-    objHero: function(obj, select) {
-        let object = new PIXI.Sprite(PIXI.loader.resources["assets/obj/hero.png"].texture);
-        object.x = obj.x;
-        object.y = obj.y-object.height;
 
-        return object
-    },
-    objLightbulb: function(obj, select) {
-        let object = new PIXI.Sprite(PIXI.loader.resources["assets/obj/lightbulb.png"].texture)
-        
-        object.width *= 2
-        object.height *= 2
+    //these are all generic - we can use the const functions above to render them
+    objHero: generic1x,
 
-        object.x = obj.x;
-        object.y = obj.y-object.height;
-
-        return object
-    },
-    objEnemy_bat: function(obj, select) {
-        let object = new PIXI.Sprite(PIXI.loader.resources["assets/obj/bat.png"].texture)
-        
-        object.width *= 2
-        object.height *= 2
-
-        object.x = obj.x;
-        object.y = obj.y-object.height;
-
-        return object
-    }
+    objSeal: generic2x,
+    objLightbulb: generic2x,
+    objEnemy_bat: generic2x
 }
